7b49ab2b18eb344aea83a923c2f8a25cbc498193
Joel Burke <jburke@bizstream.com>
2019-10-25 12:46:21 -0400

    getting the basic hello world app running on android!

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Open up App.js to start working on your app! 2</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  welcome: {
+    color: 'white',
+    fontSize: 20,
+    margin: 20
+  }
+});
+
+
